cbc90280880e8ea74ea0c90645a54d6ec363e7ea
dac <dac@dypm.org>
2019-06-26 10:01:54 +0530

    first file

---
+++ service.js
@@ -0,0 +1,23 @@
+var express=require('express');
+var app=express();
+
+var employeeController=function(req,res)
+{
+console.log("rest api");
+  var employee=[
+            {firstName:'Vrasha',lastName:'Magdum',age:22,designation:'Jr. engg'},
+            {firstName:'Dhanshri',lastName:'Deshmukh',age:22,designation:'Jr. engg'},
+			{firstName:'Priya',lastName:'Shintre',age:22,designation:'Jr. engg'},
+			{firstName:'Komal',lastName:'Gupta',age:23,designation:'Sr. engg'},
+      ];
+  res.send(employee);
+};
+
+app.get('/employee',employeeController );
+
+var server = app.listen(8088, function () {
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log("Example app listening at http://localhost:8088", host, port)
+})
